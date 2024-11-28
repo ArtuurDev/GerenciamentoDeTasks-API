ba7f31584464eb295f20923dea190753704788b3
artuur <arturcastrodossantos.com@gmail.com>
2024-11-28 20:18:13 -0300

    fazendo verificação da url com a regex e adiconado a propiedade param ao request

--- src/server.js
+++ src/server.js
@@ -1,17 +1,22 @@
 import http from 'node:http'
 import { routes } from './routes/routes.js'
-import { requestBody } from './routes/middleware/request-body.js'
+import { requestBody } from './middleware/request-body.js'
 
 const server = http.createServer(async(req, res) => {
 
     const {method, url} = req
     await requestBody(req, res)
 
-    const route = routes.find(route => route.method === method && route.urlPath === url) 
+    const route = routes.find(route => route.method === method && route.urlPath.test(url)) 
 
     if (route) {
+
+        const parameters = url.match(route.urlPath)
+
+        req.param = {...parameters.groups}
+        
         return route.handler(req, res)
-    }  
+    } 
     else {
         return res.writeHead(404).end('page not found')
     }
